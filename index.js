088a451edbe488306a991fa16845fc3260a6b3c0
guillaumebarranco <guillaume.barranco8@hotmail.fr>
2017-04-28 00:21:37 +0200

    [IMPROV] Add index vertical

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ socket.on('connect', function() {
 
 	Reveal.addEventListener('slidechanged', function(event) {
 		// event.previousSlide, event.currentSlide, event.indexh, event.indexv
-		socket.emit('slide', event.indexh);
+		socket.emit('slide', {
+			indexh: event.indexh,
+			indexv: event.indexv
+		});
 	});
 
 	socket.on('slide', function(data) {
-		Reveal.slide(data);
+		Reveal.slide(data.indexh, data.indexv);
 	});
 });
